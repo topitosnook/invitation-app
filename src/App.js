4b22e896d99f76da778084c92c7392a2a24c60bb
Valeria Argomedo <69736906+topitosnook@users.noreply.github.com>
2024-12-06 12:52:37 -0300

    changes almost done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 import sprite from "./sprite.svg";
+import WhatsAppButton from "./WhatsAppButtonComponent.js"
 
 // Import images from the local src directory
 import img1 from "./mage1/1.png";
@@ -13,38 +14,66 @@ const images = [img1, img2];
 function App() {
   return (
     <div className="box">
-      <Carousel  showIndicators
-            renderArrowNext={(clickHandler, hasNext) => {
-              return (
-                hasNext && (
-                  <button className="nav_btn nav_btn_right" onClick={clickHandler}>
-                    <svg>
-                      <use xlinkHref={sprite + "#right"}></use>
-                    </svg>
-                  </button>
-                )
-              );
-            }}
-            renderArrowPrev={(clickHandler, hasNext) => {
-              return (
-                hasNext && (
-                  <button onClick={clickHandler} className="nav_btn nav_btn_left">
-                    <svg>
-                      <use xlinkHref={sprite + "#left"}></use>
-                    </svg>
-                  </button>
-                )
-              );
-            }}
+      <Carousel showIndicators
+        renderArrowNext={(clickHandler, hasNext) => {
+          return (
+            hasNext && (
+              <button className="nav_btn nav_btn_right" onClick={clickHandler}>
+                <svg>
+                  <use xlinkHref={sprite + "#right"}></use>
+                </svg>
+              </button>
+            )
+          );
+        }}
+        renderArrowPrev={(clickHandler, hasNext) => {
+          return (
+            hasNext && (
+              <button onClick={clickHandler} className="nav_btn nav_btn_left">
+                <svg>
+                  <use xlinkHref={sprite + "#left"}></use>
+                </svg>
+              </button>
+            )
+          );
+        }}
 
-            >
+      >
         {images.map((image, index) => (
           <div className="slide-box" key={index}>
             <img alt={`slide-${index}`} src={image} />
           </div>
         ))}
       </Carousel>
+      <div className="info-box">
+        <div className="important">
+          <p>
+            Favor confirmar asistencia o pedir más información con la Vale
+          </p>
+          <p>(Franco no cacha nada) &#128540;</p>
+          <br/>
+          <WhatsAppButton />
+        </div>
+        <div className="more-info">
+          <ul className="list-info">
+            <li>
+              Para asegurar tu asistencia y estadía, pedimos una cuota de $15.000 por persona, esto nos ayuda para poder sobrevivir este gasto &#128517;
+            </li>
+            <li>
+              Vestimenta: Opcional &#127882;
+            </li>
+            <li>
+              Si llegan temprano o se van tarde habra piscina así que lleven sus cositas &#129343;  
+            </li>
+            <li>
+              La ubicación por maps será entregada más adelante &#128506;
+            </li>
+          </ul>
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
---
+++ src/WhatsAppButtonComponent.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const WhatsAppButton = () => {
+  const phoneNumber = "56995242695"; 
+  const message = "RSVP"; 
+
+  // Encode the message to handle spaces and special characters
+  const encodedMessage = encodeURIComponent(message);
+
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+  return (
+    <a
+      href={whatsappUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{
+        display: "inline-flex",
+        alignItems: "center",
+        padding: "10px 20px",
+        backgroundColor: "#21bd5b", // WhatsApp green color
+        color: "#fff",
+        borderRadius: "5px",
+        textDecoration: "none",
+        fontWeight: "bold",
+      }}
+    >
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+        alt="WhatsApp"
+        style={{ width: "20px", marginRight: "10px" }}
+      />
+      RSVP via WhatsApp
+    </a>
+  );
+};
+
+export default WhatsAppButton;
